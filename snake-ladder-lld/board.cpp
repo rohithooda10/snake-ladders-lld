706c3d018415875152aa13006b534843c0df1d09
Rohit Hooda <rhooda410@gmail.com>
2024-01-14 18:58:13 +0530

    Readme

--- snake-ladder-lld/board.cpp
+++ snake-ladder-lld/board.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 Board::Board(int size, vector<vector<int> > snakes, vector<vector<int> > ladders)
+{
+	this->size = size;
+	for(auto &snake: snakes)
 	{
-		this->size = size;
-		for(auto &snake: snakes)
-		{
-			this->snakes[snake[0]] = snake[1];
-		}
-		for(auto &ladder: ladders)
-		{
-			this->ladders[ladder[0]] = ladder[1];
-		}
+		this->snakes[snake[0]] = snake[1];
 	}
+	for(auto &ladder: ladders)
+	{
+		this->ladders[ladder[0]] = ladder[1];
+	}
+}
